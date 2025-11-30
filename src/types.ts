1e71bc580dfcd8b2876aca350e0257e856d4e53f
Eugene Kosukhin <kosuha606@gmail.com>
2025-11-30 16:12:19 +0300

    feat: validation types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+import { ConstructorType, MessageType } from "silentium";
+
+export type ValidationErrorType = Record<string, string[]>;
+
+export type ValidationRule = MessageType<string | boolean>;
+
+export interface ValidationItem {
+  value: unknown;
+  key: string;
+  rules: ConstructorType<[ValidationRule]>[];
+}
